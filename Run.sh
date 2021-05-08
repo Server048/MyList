4a4026f1cd66b06f5f5a7eb75d692e720c2db440
Server048 <64312413+Server048@users.noreply.github.com>
2021-05-08 15:30:45 +0800

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 #
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+sleep 1
 clear
 figlet -f big Termux Banner | lolcat
 echo -e $lightgreen "\e[1m                Setti>
