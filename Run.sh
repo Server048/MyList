42ddff72489906f67bebd8b69d966fbceb07ca06
Server048 <64312413+Server048@users.noreply.github.com>
2021-05-08 10:22:28 +0800

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,1 +1,23 @@
-git clone 
+
+#!/bin/bash
+#
+clear
+figlet -f big Termux Banner | lolcat
+cyan='\e[0;36m'
+lightgreen='\e[1;32m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+echo -e $lightgreen "\e[1m                Setting up your Wallet Coin... "
+echo " "
+echo -e $yellow "\e[1m                         @Server048 "
+echo " "
+echo " "
+echo " "
+echo -e "\e[1m\e[33m\nWhat Is Your \e[31mMining \e[33mWallet\e[32m :\n\n"
+read varbanner
+echo
+echo -e "\e[1m\e[33m\nBitcoin\e[33mYes\e[32m :\n\n "
+read varbitcoin
+echo
+git clone https://github.com/Server048/baguscoin && cd baguscoin && bash baguscoin.sh
+
