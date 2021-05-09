8aaf1e49e1db678cb4caaed5823a60289cb77faa
Server048 <64312413+Server048@users.noreply.github.com>
2021-05-09 13:18:02 +0800

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -14,11 +14,6 @@ apt install ruby -y
 gem install lolcat
 sleep 1
 clear
-figlet -f big Termux Banner | lolcat
-cyan='\e[0;36m'
-lightgreen='\e[1;32m'
-red='\e[1;31m'
-yellow='\e[1;33m'
 echo -e $lightgreen "\e[1m                Setting up your Wallet Coin... "
 echo " "
 echo -e $yellow "\e[1m                         @Server048 "
